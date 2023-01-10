569b992babca7c9a8a64649e7666e1441d970928
ist1102313 <manuel.sokolov.ravasqueira@tecnico.ulisboa.pt>
2023-01-10 15:57:25 +0100

    new version

--- app.R
+++ app.R
@@ -2,10 +2,28 @@ library(shiny)
 library(ggplot2)
 library(magrittr)
 library(dplyr)
+library(tidyr)
+
+
+unicorn_companies <- read.csv("data/Unicorn_Clean.csv")
+industry_values <- unique(unicorn_companies$Industry)
+print(industry_values)
+industry_choices <- unlist(industry_values)
+print(industry_choices)
+
 ui <- navbarPage(
   "My Shiny App",
   tabPanel("Tab 1 - Industry and Investors", value = "tab1",
            titlePanel("Are companies in certain industries more likely to attract certain investors?"),
+           fluidRow(
+             column(3, style = "background-color: #F4F6F6; height: 580px; width: 230px",
+                    radioButtons(
+                      inputId = "industry",
+                      h4(strong("Select a Industry Type:")), 
+                      choices = industry_choices,
+                      selected = NULL,
+                      inline = FALSE),
+             )),
            plotOutput("industry_investors_plot", click = "plot_click")),
   tabPanel("Tab 2", value = "tab2",titlePanel("Is there any geographical pattern regarding investment?"),
            plotOutput("industry_investors_plot2", click = "plot_click"),
@@ -16,25 +34,45 @@ ui <- navbarPage(
 
 server <- function(input, output) {
   # Read in the unicorn companies dataset
-  unicorn_companies <- read.csv("data/unicorn_companies.csv")
-  print(unique(unicorn_companies$Industry))
-  print(names(unicorn_companies))
+  #print(unique(unicorn_companies$Industry))
+#  print(names(unicorn_companies))
+  #print(input$industry)
+  industry_select <- reactive({input$industry})
+  print(industry_select)
+  #output$industry_investors_plot <- renderPlot({
+  #  industry_investors_data <- unicorn_companies %>% 
+  #    select(Industry, Company)
+  #  industry_investors_data <- reshape2::melt(industry_investors_data, id.vars = "Company")
+  #  ggplot(data = industry_investors_data, aes(x=Company, y=value, fill = variable)) + 
+  #    geom_tile() + 
+  #    scale_fill_manual(values = c("#999999", "#E69F00")) +
+  #    theme_minimal() +
+  #    ggtitle("Industry and Country correlation plot")
+  #})
   
   output$industry_investors_plot <- renderPlot({
     industry_investors_data <- unicorn_companies %>% 
-      select(Industry, Company)
-    industry_investors_data <- reshape2::melt(industry_investors_data, id.vars = "Company")
-    ggplot(data = industry_investors_data, aes(x=Company, y=value, fill = variable)) + 
-      geom_tile() + 
-      scale_fill_manual(values = c("#999999", "#E69F00")) +
-      theme_minimal() +
-      ggtitle("Industry and Country correlation plot")
+      select(Industry, City)
+    filtered_data <- unicorn_companies %>% filter(Industry == industry_select())
+    investors_data <- filtered_data %>% 
+      gather("Investor", "name", Investor.1:Investor.4) %>%
+      group_by(name) %>%
+      summarise(n=n()) %>%
+      top_n(10)
+    print(investors_data)
+    ggplot(data = investors_data, aes(x=name, y=n, fill = name)) + 
+      geom_bar(stat = "identity") +
+      ggtitle("Top Investors for") +
+      xlab("Investor") +
+      ylab("Correlation")
+    
   })
+  
   output$industry_investors_plot2 <- renderPlot({
     industry_investors_data <- unicorn_companies %>% 
       select(Industry, City)
     industry_investors_data <- reshape2::melt(industry_investors_data, id.vars = "City")
-    ggplot(data = industry_investors_data, aes(x=City, y=value, fill = variable)) + 
+    ggplot(data = industry_investors_data, aes(x=City, y=value, filal = variable)) + 
       geom_tile() + 
       scale_fill_manual(values = c("#999999", "#E69F00")) +
       theme_minimal() +
